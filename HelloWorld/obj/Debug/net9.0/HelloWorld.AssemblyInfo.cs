9ac6d765f79a96c057550a7f3b938cffd0a62ce2
Kipp Jennings <jenningskipp@gmail.com>
2024-12-06 20:25:10 -0600

    I guess?

--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e85bfcc4f3514ebabf8538dc77638fec85ddd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26459b0d1e0338922ba2a7d599557461c40cfb1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
